0174a1da31d008ea01bb0354ee14d718a1c8a7d1
Marat Dulin <mdevils@yandex-team.ru>
2013-05-21 14:45:20 +0400

    Version 0.1.8

--- test/test.cls.js
+++ test/test.cls.js
@@ -18,6 +18,24 @@ describe('ctx.cls()', function() {
         });
         bh.apply({ block: 'button' }).should.equal('<div class="button btn"></div>');
     });
+    it('should escape cls', function() {
+        bh.match('button', function(ctx) {
+            ctx.cls('btn"1');
+        });
+        bh.apply({ block: 'button' }).should.equal('<div class="button btn&quot;1"></div>');
+    });
+    it('should escape BEM cls', function() {
+        bh.match('button"1', function(ctx) {
+            ctx.cls('btn"1');
+        });
+        bh.match('button"1__elem"1', function(ctx) {
+            ctx.cls('elem"1');
+        });
+        bh.apply({ block: 'button"1', content: {elem: 'elem"1'} })
+            .should.equal(
+                '<div class="button&quot;1 btn&quot;1"><div class="button&quot;1__elem&quot;1 elem&quot;1"></div></div>'
+            );
+    });
     it('should not override user cls', function() {
         bh.match('button', function(ctx) {
             ctx.cls('btn');
