72f6764ac8b3defb2f4e9b96e0d9b049a11e994e
Marat Dulin <mdevils@yandex-team.ru>
2013-06-05 17:44:16 +0400

    Version 0.1.11: Attrs mods with null values

---
+++ test/test.toHtml.js
@@ -0,0 +1,65 @@
+var BH = require('../lib/bh');
+require('chai').should();
+
+describe('bh.toHtml()', function() {
+    describe('attrs', function() {
+        var bh;
+        beforeEach(function() {
+            bh = new BH();
+        });
+        it('should ignore null attrs', function() {
+            bh.match('button', function(ctx) {
+                ctx.tag('a');
+                ctx.attr('href', '#');
+            });
+            bh.match('button', function(ctx) {
+                ctx.attr('href', null);
+            });
+            bh.apply({ block: 'button' }).should.equal(
+                '<a class="button"></a>'
+            );
+        });
+        it('should not ignore empty attrs', function() {
+            bh.match('button', function(ctx) {
+                ctx.tag('a');
+                ctx.attr('href', '#');
+            });
+            bh.match('button', function(ctx) {
+                ctx.attr('href', '');
+            });
+            bh.apply({ block: 'button' }).should.equal(
+                '<a class="button" href=""></a>'
+            );
+        });
+    });
+    describe('mods', function() {
+        var bh;
+        beforeEach(function() {
+            bh = new BH();
+        });
+        it('should ignore null mods', function() {
+            bh.match('button', function(ctx) {
+                ctx.tag('a');
+                ctx.mod('type', 'active');
+            });
+            bh.match('button', function(ctx) {
+                ctx.mod('type', null);
+            });
+            bh.apply({ block: 'button' }).should.equal(
+                '<a class="button"></a>'
+            );
+        });
+        it('should ignore empty mods', function() {
+            bh.match('button', function(ctx) {
+                ctx.tag('a');
+                ctx.mod('type', 'active');
+            });
+            bh.match('button', function(ctx) {
+                ctx.mod('type', '');
+            });
+            bh.apply({ block: 'button' }).should.equal(
+                '<a class="button"></a>'
+            );
+        });
+    });
+});
