b84977bf424b26ba719b7d6f383092fdcef37bfe
Max Devils <mdevils@yandex.ru>
2013-06-26 17:14:27 +0400

    Update bh-client.js

--- techs/bh-client.js
+++ techs/bh-client.js
@@ -1,4 +1,22 @@
-var Vow = require('vow'),
+/**
+ * bh-client
+ * =========
+ * 
+ * Склеивает *bh*-файлы по deps'ам с помощью набора `require` в виде `?.bemhtml.client.js`.
+ * Предназначен для сборки клиентского BH-кода.
+ * 
+ * **Опции**
+ * 
+ * * *String* **target** — Результирующий таргет. По умолчанию — `?.bemhtml.client.js`.
+ * * *String* **filesTarget** — files-таргет, на основе которого получается список исходных файлов (его предоставляет технология `files`). По умолчанию — `?.files`.
+ * 
+ * **Пример**
+ * 
+ * ```javascript
+ * nodeConfig.addTech(require('bh/techs/bh-client'));
+ * ```
+ */
+ var Vow = require('vow'),
     vowFs = require('vow-fs'),
     bhClientProcessor = require('../lib/bh-client-processor');
 
@@ -8,6 +26,9 @@ module.exports = require('enb/lib/build-flow').create()
     .defineOption('bhFile', '')
     .defineOption('dependencies', {})
     .useFileList(['bh.js'])
+    /**
+     * Отдельно кэшируем BH-библиотеку.
+     */
     .needRebuild(function(cache) {
         this._bhFile = this._bhFile || 'node_modules/bh/lib/bh.js';
         this._bhFile = this.node._root + '/' + this._bhFile;
@@ -26,6 +47,9 @@ module.exports = require('enb/lib/build-flow').create()
             Vow.all(bhFiles.map(function(file) {
                 return vowFs.read(file.fullname, 'utf8').then(function(data) {
                     var relPath = node.relativePath(file.fullname);
+                    /**
+                     * Выставляем комментарии о склеенных файлах.
+                     */
                     return '// begin: ' + relPath + '\n'
                         + bhClientProcessor.process(data) + '\n'
                         + '// end: ' + relPath + '\n';
