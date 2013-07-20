11da4d3432ef66c2aa2e20497b1dfec8db19daa2
ikokostya <ikokostya@gmail.com>
2013-07-20 13:09:54 +0400

    rename processBemjson -> processBemJson

--- test/test.param.js
+++ test/test.param.js
@@ -16,13 +16,13 @@ describe('ctx.param()', function() {
         bh.match('button', function(ctx) {
             ctx.param('type', 'button');
         });
-        bh.processBemjson({ block: 'button' }).type.should.equal('button');
+        bh.processBemJson({ block: 'button' }).type.should.equal('button');
     });
     it('should not override user param', function() {
         bh.match('button', function(ctx) {
             ctx.param('type', 'button');
         });
-        bh.processBemjson({ block: 'button', type: 'link' }).type.should.equal('link');
+        bh.processBemJson({ block: 'button', type: 'link' }).type.should.equal('link');
     });
     it('should not override later declarations', function() {
         bh.match('button', function(ctx) {
@@ -31,7 +31,7 @@ describe('ctx.param()', function() {
         bh.match('button', function(ctx) {
             ctx.param('type', 'button');
         });
-        bh.processBemjson({ block: 'button' }).type.should.equal('button');
+        bh.processBemJson({ block: 'button' }).type.should.equal('button');
     });
     it('should override later declarations with force flag', function() {
         bh.match('button', function(ctx) {
@@ -40,12 +40,12 @@ describe('ctx.param()', function() {
         bh.match('button', function(ctx) {
             ctx.param('type', 'button');
         });
-        bh.processBemjson({ block: 'button' }).type.should.equal('control');
+        bh.processBemJson({ block: 'button' }).type.should.equal('control');
     });
     it('should override user declarations with force flag', function() {
         bh.match('button', function(ctx) {
             ctx.param('type', 'button', true);
         });
-        bh.processBemjson({ block: 'button', type: 'link' }).type.should.equal('button');
+        bh.processBemJson({ block: 'button', type: 'link' }).type.should.equal('button');
     });
 });
