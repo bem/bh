c18ca777c5a80a5141e6d9000c6a36776d396f7a
Mikhail Troshev <mishanga@yandex-team.ru>
2014-06-23 10:15:56 +0400

    Refactor escaping

--- test/test.escape.js
+++ test/test.escape.js
@@ -7,49 +7,11 @@ describe('bh.xmlEscape()', function() {
         bh = new BH();
     });
 
-    it('should match and make unescape xml string: &amp', function() {
-        bh.match('suggest-item', function(ctx) {
-            ctx.content(bh.xmlEscape(ctx.content()), true);
+    it('should escape xml string', function() {
+        bh.match('button', function(ctx) {
+            bh.xmlEscape('<b>&</b>').should.equal('&lt;b&gt;&amp;&lt;/b&gt;');
         });
-
-        bh.apply({
-            block: 'suggest-item',
-            content: "a&b"
-        }).should.equal('<div class="suggest-item">a&amp;b</div>');
-    });
-
-    it('should match and make unescape xml string: &lt', function() {
-        bh.match('suggest-item', function(ctx) {
-            ctx.content(bh.xmlEscape(ctx.content()), true);
-        });
-
-        bh.apply({
-            block: 'suggest-item',
-            content: "<"
-        }).should.equal('<div class="suggest-item">&lt;</div>');
-    });
-
-    it('should match and make unescape xml string: &gt', function() {
-        bh.match('suggest-item', function(ctx) {
-            ctx.content(bh.xmlEscape(ctx.content()), true);
-        });
-
-        bh.apply({
-            block: 'suggest-item',
-            content: ">"
-        }).should.equal('<div class="suggest-item">&gt;</div>');
-    });
-
-    it('should match and make unescape xml string: (&amp, &lt, &gt)', function() {
-        bh.match('suggest-item', function(ctx) {
-            ctx.content(bh.xmlEscape(ctx.content()), true);
-        });
-
-        var xml = '<?xml version="1&0" ?>';
-        bh.apply({
-            block: 'suggest-item',
-            content: xml
-        }).should.equal('<div class="suggest-item">&lt;?xml version="1&amp;0" ?&gt;</div>');
+        bh.apply({ block: 'button' });
     });
 });
 
@@ -59,60 +21,10 @@ describe('bh.attrEscape()', function() {
         bh = new BH();
     });
 
-    it('should match and make unescape atrr string: &amp', function() {
-        bh.match('suggest', function(ctx) {
-            ctx.content(bh.attrEscape(ctx.content()), true);
+    it('should escape xml attr string', function() {
+        bh.match('button', function(ctx) {
+            bh.attrEscape('<b id="a">&</b>').should.equal('&lt;b id=&quot;a&quot;&gt;&amp;&lt;/b&gt;');
         });
-
-        bh.apply({
-            block: 'suggest',
-            content: 'a&b'
-        }).should.equal('<div class="suggest">a&amp;b</div>');
-    });
-
-    it('should match and make unescape atrr string: &quot', function() {
-        bh.match('suggest', function(ctx) {
-            ctx.content(bh.attrEscape(ctx.content()), true);
-        });
-
-        bh.apply({
-            block: 'suggest',
-            content: 'a"b'
-        }).should.equal('<div class="suggest">a&quot;b</div>');
-    });
-
-    it('should match and make unescape atrr string: &lt', function() {
-        bh.match('suggest', function(ctx) {
-            ctx.content(bh.attrEscape(ctx.content()), true);
-        });
-
-        bh.apply({
-            block: 'suggest',
-            content: 'a<b'
-        }).should.equal('<div class="suggest">a&lt;b</div>');
-    });
-
-    it('should match and make unescape atrr string: &gt', function() {
-        bh.match('suggest', function(ctx) {
-            ctx.content(bh.attrEscape(ctx.content()), true);
-        });
-
-        bh.apply({
-            block: 'suggest',
-            content: 'a>b'
-        }).should.equal('<div class="suggest">a&gt;b</div>');
-    });
-
-
-    it('should match and make unescape atrr string: (&quot, &amp, &lt, &gt)', function() {
-        bh.match('suggest', function(ctx) {
-            ctx.content(bh.attrEscape(ctx.content()), true);
-        });
-
-        var xml = '<?xml version="1&0" ?>';
-        bh.apply({
-            block: 'suggest',
-            content: xml
-        }).should.equal('<div class="suggest">&lt;?xml version=&quot;1&amp;0&quot; ?&gt;</div>');
+        bh.apply({ block: 'button' });
     });
 });
