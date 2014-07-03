f3ffd89af3e8720f2cf68548ec42f0a747739b4d
Mikhail Troshev <mishanga@yandex-team.ru>
2014-07-03 16:23:45 +0400

    attr: consider undefined values

--- test/test.attr.js
+++ test/test.attr.js
@@ -6,18 +6,21 @@ describe('ctx.attr()', function() {
     beforeEach(function() {
         bh = new BH();
     });
+
     it('should return attr', function() {
         bh.match('button', function(ctx) {
             ctx.attr('type').should.equal('button');
         });
         bh.apply({ block: 'button', attrs: { type: 'button' } });
     });
+
     it('should return undefined attr', function() {
         bh.match('button', function(ctx) {
             (typeof ctx.attr('type')).should.equal('undefined');
         });
         bh.apply({ block: 'button', attrs: { disabled: 'disabled' } });
     });
+
     it('should set attr', function() {
         bh.match('checkbox', function(ctx) {
             ctx.attr('type', 'button');
@@ -27,12 +30,21 @@ describe('ctx.attr()', function() {
         });
         bh.apply({ block: 'checkbox' }).should.equal('<div class="checkbox" type="button" disabled="false"></div>');
     });
+
     it('should not override user attr', function() {
         bh.match('button', function(ctx) {
             ctx.attr('type', 'button');
+            ctx.attr('disabled', true);
         });
-        bh.apply({ block: 'button', attrs: { type: 'link' } }).should.equal('<div class="button" type="link"></div>');
+        bh.apply({
+            block: 'button',
+            attrs: {
+                type: 'link',
+                disabled: undefined
+            }
+        }).should.equal('<div class="button" type="link"></div>');
     });
+
     it('should not override later declarations', function() {
         bh.match('button', function(ctx) {
             ctx.attr('type', 'control');
@@ -42,6 +54,7 @@ describe('ctx.attr()', function() {
         });
         bh.apply({ block: 'button' }).should.equal('<div class="button" type="button"></div>');
     });
+
     it('should override later declarations with force flag', function() {
         bh.match('button', function(ctx) {
             ctx.attr('type', 'control', true);
@@ -51,10 +64,18 @@ describe('ctx.attr()', function() {
         });
         bh.apply({ block: 'button' }).should.equal('<div class="button" type="control"></div>');
     });
+
     it('should override user declarations with force flag', function() {
         bh.match('button', function(ctx) {
             ctx.attr('type', 'button', true);
+            ctx.attr('disabled', undefined, true);
         });
-        bh.apply({ block: 'button', attrs: { type: 'link' } }).should.equal('<div class="button" type="button"></div>');
+        bh.apply({
+            block: 'button',
+            attrs: {
+                type: 'link',
+                disabled: 'disabled'
+            }
+        }).should.equal('<div class="button" type="button"></div>');
     });
 });
