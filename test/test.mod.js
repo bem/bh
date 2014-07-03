bb4ced6351dbfda1b97500b63c04e892312843d7
Mikhail Troshev <mishanga@yandex-team.ru>
2014-07-03 16:58:29 +0400

    mod: consider undefined values

--- test/test.mod.js
+++ test/test.mod.js
@@ -67,64 +67,87 @@ describe('ctx.mod()', function() {
     beforeEach(function() {
         bh = new BH();
     });
+
     it('should return mod', function() {
-        bh.match('component', function(ctx) {
+        bh.match('button', function(ctx) {
             ctx.mod('type').should.equal('button');
         });
-        bh.apply({ block: 'component', mods: { type: 'button' } });
+        bh.apply({ block: 'button', mods: { type: 'button' } });
     });
+
     it('should return undefined mod', function() {
-        bh.match('component', function(ctx) {
+        bh.match('button', function(ctx) {
             (typeof ctx.mod('type')).should.equal('undefined');
         });
-        bh.apply({ block: 'component', mods: { disabled: true } });
+        bh.apply({ block: 'button', mods: { disabled: true } });
     });
+
     it('should return boolean mod', function() {
-        bh.match('component', function(ctx) {
+        bh.match('button', function(ctx) {
             ctx.mod('disabled').should.equal(true);
         });
-        bh.apply({ block: 'component', mods: { disabled: true } });
+        bh.apply({ block: 'button', mods: { disabled: true } });
     });
+
     it('should set mod', function() {
-        bh.match('component', function(ctx) {
+        bh.match('button', function(ctx) {
             ctx.mod('type', 'button');
         });
-        bh.apply({ block: 'component' }).should.equal('<div class="component component_type_button"></div>');
+        bh.apply({ block: 'button' }).should.equal('<div class="button button_type_button"></div>');
     });
+
     it('should set boolean mod', function() {
-        bh.match('component', function(ctx) {
+        bh.match('button', function(ctx) {
             ctx.mod('disabled', true);
         });
-        bh.apply({ block: 'component' }).should.equal('<div class="component component_disabled"></div>');
+        bh.apply({ block: 'button' }).should.equal('<div class="button button_disabled"></div>');
     });
+
     it('should not override user mod', function() {
-        bh.match('component', function(ctx) {
+        bh.match('button', function(ctx) {
             ctx.mod('type', 'button');
+            ctx.mod('disabled', true);
         });
-        bh.apply({ block: 'component', mods: { type: 'link' } }).should.equal('<div class="component component_type_link"></div>');
+        bh.apply({
+            block: 'button',
+            mods: {
+                type: 'link',
+                disabled: undefined
+            }
+        }).should.equal('<div class="button button_type_link"></div>');
     });
+
     it('should not override later declarations', function() {
-        bh.match('component', function(ctx) {
+        bh.match('button', function(ctx) {
             ctx.mod('type', 'control');
         });
-        bh.match('component', function(ctx) {
+        bh.match('button', function(ctx) {
             ctx.mod('type', 'button');
         });
-        bh.apply({ block: 'component' }).should.equal('<div class="component component_type_button"></div>');
+        bh.apply({ block: 'button' }).should.equal('<div class="button button_type_button"></div>');
     });
+
     it('should override later declarations with force flag', function() {
-        bh.match('component', function(ctx) {
+        bh.match('button', function(ctx) {
             ctx.mod('type', 'control', true);
         });
-        bh.match('component', function(ctx) {
+        bh.match('button', function(ctx) {
             ctx.mod('type', 'button');
         });
-        bh.apply({ block: 'component' }).should.equal('<div class="component component_type_control"></div>');
+        bh.apply({ block: 'button' }).should.equal('<div class="button button_type_control"></div>');
     });
+
     it('should override user declarations with force flag', function() {
-        bh.match('component', function(ctx) {
+        bh.match('button', function(ctx) {
             ctx.mod('type', 'button', true);
+            ctx.mod('disabled', undefined, true);
         });
-        bh.apply({ block: 'component', mods: { type: 'link' } }).should.equal('<div class="component component_type_button"></div>');
+        bh.apply({
+            block: 'button',
+            mods: {
+                type: 'link',
+                disabled: true
+            }
+        }).should.equal('<div class="button button_type_button"></div>');
     });
 });
