34ef805ab6d9593bfa6a05d70c605b219699880b
Mikhail Troshev <mishanga@yandex-team.ru>
2014-08-05 23:26:12 +0400

    Mixed items should inherit elem name (fix #69)

--- test/test.mix.js
+++ test/test.mix.js
@@ -6,6 +6,7 @@ describe('ctx.mix()', function() {
     beforeEach(function() {
         bh = new BH();
     });
+
     it('should return mix', function() {
         var mix = [{ block: 'mix' }];
         bh.match('button', function(ctx) {
@@ -13,30 +14,35 @@ describe('ctx.mix()', function() {
         });
         bh.apply({ block: 'button', mix: mix });
     });
+
     it('should set mix', function() {
         bh.match('button', function(ctx) {
             ctx.mix([{ block: 'mix' }]);
         });
         bh.apply({ block: 'button' }).should.equal('<div class="button mix"></div>');
     });
+
     it('should set single mix', function() {
         bh.match('button', function(ctx) {
             ctx.mix({ block: 'mix' });
         });
         bh.apply({ block: 'button' }).should.equal('<div class="button mix"></div>');
     });
+
     it('should extend single mix', function() {
         bh.match('button', function(ctx) {
             ctx.mix({ block: 'mix2' });
         });
         bh.apply({ block: 'button', mix: { block: 'mix1' } }).should.equal('<div class="button mix1 mix2"></div>');
     });
+
     it('should extend user mix', function() {
         bh.match('button', function(ctx) {
             ctx.mix([{ block: 'mix' }]);
         });
         bh.apply({ block: 'button', mix: [{ block: 'user-mix' }] }).should.equal('<div class="button user-mix mix"></div>');
     });
+
     it('should extend later declarations', function() {
         bh.match('button', function(ctx) {
             ctx.mix([{ block: 'mix2' }]);
@@ -46,6 +52,7 @@ describe('ctx.mix()', function() {
         });
         bh.apply({ block: 'button' }).should.equal('<div class="button mix1 mix2"></div>');
     });
+
     it('should override later declarations with force flag', function() {
         bh.match('button', function(ctx) {
             ctx.mix([{ block: 'mix2' }], true);
@@ -55,10 +62,37 @@ describe('ctx.mix()', function() {
         });
         bh.apply({ block: 'button' }).should.equal('<div class="button mix2"></div>');
     });
+
     it('should override user declarations with force flag', function() {
         bh.match('button', function(ctx) {
             ctx.mix([{ block: 'mix' }], true);
         });
         bh.apply({ block: 'button', mix: [{ block: 'user-mix' }] }).should.equal('<div class="button mix"></div>');
+    });
+
+    it('should inherit block name', function() {
+        bh.match('button', function(ctx) {
+            ctx.mix([
+                { mods: { disabled: true } },
+                { elem: 'input', mods: { active: true } },
+                { block: 'clearfix' }
+            ]);
+        });
+        bh.apply({ block: 'button' }).should.equal(
+            '<div class="button button_disabled button__input button__input_active clearfix"></div>'
+        );
+    });
+
+    it('should inherit element name', function() {
+        bh.match('button__control', function(ctx) {
+            ctx.mix([
+                { mods: { disabled: true } },
+                { elem: 'input', mods: { active: true } },
+                { block: 'clearfix' }
+            ]);
+        });
+        bh.apply({ block: 'button', elem: 'control' }).should.equal(
+            '<div class="button__control button__control_disabled button__input button__input_active clearfix"></div>'
+        );
     });
 });
