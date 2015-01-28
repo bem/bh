e4d41c4d72ea2e0d2bbd4a4c21c9a0f12a2c902e
Mikhail Troshev <mishanga@yandex-team.ru>
2015-01-28 12:49:20 +0300

    Revert "Do not add `i-bem` class to element with `js`"

--- test/test.js.js
+++ test/test.js.js
@@ -6,28 +6,24 @@ describe('ctx.js()', function() {
     beforeEach(function() {
         bh = new BH();
     });
-
     it('should return js', function() {
         bh.match('button', function(ctx) {
             ctx.js().should.equal(true);
         });
         bh.apply({ block: 'button', js: true });
     });
-
     it('should set js', function() {
         bh.match('button', function(ctx) {
             ctx.js(true);
         });
         bh.apply({ block: 'button' }).should.equal('<div class="button i-bem" onclick="return {&quot;button&quot;:{}}"></div>');
     });
-
     it('should not set js', function() {
         bh.match('button', function(ctx) {
             ctx.js(false);
         });
         bh.apply({ block: 'button' }).should.equal('<div class="button"></div>');
     });
-
     it('should extend user js', function() {
         bh.match('button', function(ctx) {
             ctx.js({ a: 2 });
@@ -35,7 +31,6 @@ describe('ctx.js()', function() {
         bh.apply({ block: 'button', js: { x: 1 } })
             .should.equal('<div class="button i-bem" onclick="return {&quot;button&quot;:{&quot;x&quot;:1,&quot;a&quot;:2}}"></div>');
     });
-
     it('should not override later declarations', function() {
         bh.match('button', function(ctx) {
             ctx.js(false);
@@ -45,7 +40,6 @@ describe('ctx.js()', function() {
         });
         bh.apply({ block: 'button' }).should.equal('<div class="button i-bem" onclick="return {&quot;button&quot;:{}}"></div>');
     });
-
     it('should override later declarations with force flag', function() {
         bh.match('button', function(ctx) {
             ctx.js(true, true);
@@ -55,7 +49,6 @@ describe('ctx.js()', function() {
         });
         bh.apply({ block: 'button' }).should.equal('<div class="button i-bem" onclick="return {&quot;button&quot;:{}}"></div>');
     });
-
     it('should override user declarations with force flag', function() {
         bh.match('button', function(ctx) {
             ctx.js(false, true);
